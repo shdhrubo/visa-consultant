c5f573f6775d0deca8fe557ce0e804d46e122174
shdhrubo <shorifulhabib.iit@gmail.com>
2022-04-18 03:09:49 +0600

    services added

---
+++ src/components/Service/Service.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Service = ({ service }) => {
+  const { id, name, img, description, price } = service;
+  const navigate = useNavigate();
+
+  const navigateToServiceDetail = (id) => {
+    navigate(`/service/${id}`);
+  };
+  return (
+    <div className="col-sm-12 col-12 col-md-6 col-lg-6 border border-1 p-2">
+      <h2>{name}</h2>
+      <img className="w-25" src={img} alt="" />
+      <p>Price: {price}</p>
+      <p>
+        <small>{description}</small>
+      </p>
+      <button
+        onClick={() => navigateToServiceDetail(id)}
+        className="btn btn-outline-danger "
+      >
+        Book Your Time
+      </button>
+    </div>
+  );
+};
+
+export default Service;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div>
-      <h2 className="text-danger">Services I provide</h2>
+      <h2 id="services" className="text-danger m-3">
+        Services I provide
+      </h2>
+      <div className="container row mx-auto g-3">
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
