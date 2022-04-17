317bcb0c97e585a1ea416494bbd092e307628b83
shdhrubo <shorifulhabib.iit@gmail.com>
2022-04-17 23:39:28 +0600

    banner added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>this is header</h2>
-        </div>
-    );
+  return (
+    <Navbar bg="danger" variant="dark">
+      <Container>
+        <Navbar.Brand href="/home">Visa Consultant</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="home#services">Services</Nav.Link>
+          <Nav.Link href="home#reviews">Reviews</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
