bb09629e9f311cfbb070d6708ac0187ca7082088
shdhrubo <shorifulhabib.iit@gmail.com>
2022-04-17 22:59:32 +0600

    some components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
+import Services from "./components/Services/Services";
+import Reviews from "./components/Reviews/Reviews";
+import About from "./components/About/About";
+import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
+      <Banner></Banner>
+      <Services></Services>
+      <Reviews></Reviews>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <small>Copyright...All rights reserved by SHD</small>
+    </div>
+  );
+};
+
+export default Footer;
